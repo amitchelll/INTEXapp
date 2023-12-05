6d93db3989b52c6c500338d7eb35c860b2dc21b8
codynetty <codynetty@gmail.com>
2023-12-05 09:42:03 -0700

    put in app stuff

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const port = 3000;
 
 app.use(express.urlencoded({extended: true}));
 
+app.set("view engine", "ejs")
+
+const knex = require("knex")({ // this is the database
+    client: "pg",
+    connection: {
+        host: process.env.RDS_HOSTNAME || "localhost", 
+        user: process.env.RDS_USERNAME || "postgres",
+        password: process.env.RDS_PASSWORD || "Ptennis1",
+        database: process.env.RDS_DB_NAME || "survey",
+        port: process.env.RDS_PORT || 5432,
+        ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
+    }
+})
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/index.html"));
 });
