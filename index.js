8c56ee559d74e9dfedf1aac90a7b9825a3dc24d4
elisepickett <pickettelise0@gmail.com>
2023-12-06 09:29:51 -0700

    new password

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const knex = require("knex")({ // this is the database
     connection: {
         host: process.env.RDS_HOSTNAME || "localhost", 
         user: process.env.RDS_USERNAME || "postgres",
-        password: process.env.RDS_PASSWORD || "danielle1",
+        password: process.env.RDS_PASSWORD || "password",
         database: process.env.RDS_DB_NAME || "ebdb",
         port: process.env.RDS_PORT || 5432,
         ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
