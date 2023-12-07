18213ecf0d21e89f3e8c0dc9645bfbc70f66e4ac
amitchelll <131500645+amitchelll@users.noreply.github.com>
2023-12-07 01:33:40 -0700

    change

--- index.js
+++ index.js
@@ -103,8 +103,8 @@ module.exports = initialize
 const knex = require("knex")({ // this is the database
         client: "pg",
         connection: {
-            host: process.env.RDS_HOSTNAME || "awseb-e-xug8fmx3mp-stack-awsebrdsdatabase-ja4sr9kkfspr.c5d3qntj7b7x.us-east-1.rds.amazonaws.com", 
-            user: process.env.RDS_USERNAME || "ebroot",
+            host: process.env.RDS_HOSTNAME || "localhost", 
+            user: process.env.RDS_USERNAME || "postgres",
             password: process.env.RDS_PASSWORD || "password",
             database: process.env.RDS_DB_NAME || "ebdb",
             port: process.env.RDS_PORT || 5432,
