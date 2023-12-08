bc0050d45785a2872cee4d76cb5e34f565b560ab
elisepickett <pickettelise0@gmail.com>
2023-12-08 00:06:25 -0700

    small changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let app = express();
 
 let path = require("path");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 
 const users = [{ username: 'admin', password: 'adminpassword'}];
 
